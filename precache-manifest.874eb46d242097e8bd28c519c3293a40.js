c16faf68dfb5993e702c50177353b420b423dbfa
larrym996 <larrym996@gmail.com>
2020-05-16 08:16:39 -0400

    Updates

---
+++ precache-manifest.874eb46d242097e8bd28c519c3293a40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dda81c7e4f3a4b53a4dd8e6d38d6adb",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "23f03ed55ad85bca058d",
+    "url": "/myreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61f233c12b27ee332213",
+    "url": "/myreact/static/js/2.736ef6c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreact/static/js/2.736ef6c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f03ed55ad85bca058d",
+    "url": "/myreact/static/js/main.75dedcfd.chunk.js"
+  },
+  {
+    "revision": "c47d24e3a5ab50b9fac7",
+    "url": "/myreact/static/js/runtime-main.4c67b5c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
